c3b2b04d9f15e57557574314fd059a67247a08c2
eliamartani <eliamartani@gmail.com>
2024-03-11 22:02:01 +0000

    feat: decrease a little bit the scale of section element

--- src/components/SectionCard.vue
+++ src/components/SectionCard.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="bg-slate-50 box-border mt-5 p-8 rounded-md shadow-md w-full md:max-w-3xl md:mx-auto md:rounded-lg md:shadow-xl transition-transform hover:scale-110"
+    class="bg-slate-50 box-border mt-5 p-8 rounded-md shadow-md w-full md:max-w-3xl md:mx-auto md:rounded-lg md:shadow-xl transition-transform hover:scale-105"
   >
     <slot />
   </section>
